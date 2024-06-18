b9b698aae0c4c4d09e66cd367bd5ccdf296f4efa
root <root@localhost.localdomain>
2024-06-18 17:51:15 +0000

    update

---
+++ pacman/etc/skel/suse/.config/geany/filedefs/filetypes.c
@@ -0,0 +1,7 @@
+[build-menu]
+FT_00_LB=_Compile
+FT_00_CM=gcc -I /home/tux/Desktop/local/EFI/efo/c-efi/src/ -c "%f"
+FT_00_WD=
+FT_01_LB=_Build
+FT_01_CM=gcc -I /home/tux/Desktop/local/EFI/efo/c-efi/src/ -Wall -o "%e" "%f"
+FT_01_WD=
